e55a406fe8b75d661cc5cd27c94a4189b60dfb2a
piperod <ivan.rodriguez5@upr.edu>
2020-05-26 10:25:17 -0400

    adding sponsors

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def home():
     data = _data()
     data["readme"] = open("README.md").read()
     data["committee"] = site_data["committee"]["committee"]
+    data["config"]["sponsors"] = data["config"]["sponsors"]
     return render_template("index.html", **data)
 
 
